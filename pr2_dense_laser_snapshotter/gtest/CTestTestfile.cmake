73d5bc23a53eec813c15c5d48a4ca2e130177e12
TheDash <noobaca2@gmail.com>
2014-09-11 11:30:40 -0400

    Trimmed the branch to only the laser_joint_processor sub directory

--- pr2_dense_laser_snapshotter/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/marco/overlay_ws/src/pr2_calibration/pr2_dense_laser_snapshotter/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
